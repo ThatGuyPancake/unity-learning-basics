95ffcf3977535e1c9dc1defec93f372e0b0e0c1f
Patrick <mihailapatrick@gmail.com>
2020-07-17 20:49:07 +0200

    finish driving project (1.4)

--- Unity_CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unity_CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 10.0f;
+    private float turnSpeed = 25f;
+    private float horizontalInput;
+    private float forwardInput;
     
     // Start is called before the first frame update
     void Start()
@@ -22,8 +22,9 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
 
         //Move the vehicle forward
-        // transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward * (Time.deltaTime * speed * forwardInput));
-        transform.Translate(Vector3.right * (Time.deltaTime * turnSpeed * horizontalInput));
+        
+        //Turn vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
